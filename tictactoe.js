168e1ee569b744d2d2da7f4000c8c1aa273dadcf
Natalie Martell <nrmartell@gmail.com>
2015-09-09 14:29:52 -0500

    added buttons to players, registers in console log

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,47 @@
+$(document).ready(function(){
 
+	alert ("pick a player");
+//button clicks!
+ 	$("#playerOne").click(function(){
+   		 alert( "You are Player one")
+    	 console.log("playerOne");
+    	});
+
+	$("#playerTwo").click(function(){
+   		 
+    	 console.log("playerTwo");
+    	});
+
+	$("#newGame").click(function(){
+   		 
+    	 console.log("newGame");
+    	});
+
+
+
+
+
+
+
+/*var firstTurn= 'X'
+
+if (this.id == "boxzero")
+ {
+      if(document.getElementById("boxzero").innerHTML == ""){ 
+           document.getElementById("boxzero").innerHTML = firstTurn;
+           changeTurn();
+           console.log("hey it clicked");
+      }
+ }  
+
+ function nextTurn(){
+      if(nextTurn == 'X'){
+           nextTurn = 'O';
+      } else {
+           nextTurn = 'X';
+      } 
+ } */
+
+
+
+ });/*closing document ready*/
