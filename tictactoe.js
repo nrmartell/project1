fd43f3d6e34cda5098920f86c5b0cc6468fd02d8
Natalie Martell <nrmartell@gmail.com>
2015-09-10 10:39:48 -0500

    before I mess it up

--- tictactoe.js
+++ tictactoe.js
@@ -24,41 +24,27 @@ var cell;
 var whosNext= 'X';
 //if player tries to select a box with something in it they will get an error.
 function cellClick(cell){
-if (cell.textContent === 'X' || cell.textContent === 'O')
-{ alert ('This one already has something in it! Pick another one.');
-return
-}
-cell.textContent = whosNext;
-nextTurn();
-
+	if (cell.textContent === 'X' || cell.textContent === 'O')
+		{ alert ('This one already has something in it! Pick another one.');
+		return
+	}
+	cell.textContent = whosNext;
+	nextTurn();
 }
 function nextTurn(){
-if(whosNext == 'X'){
-    whosNext = 'O';
+	if(whosNext == 'X'){
+    	whosNext = 'O';
     }
-else {
+	else {
     whosNext = 'X';
     }
 }
-
-$(document).ready(function(){
-// we will need to find winner, if there are not 1 row of x and 1 row of o somewhere on the board we will need to alert that it is a cats game
-var playerOne= 'X';
-var playerTwo= 'O';
-
-
-function getWinner(playerOne,playerTwo){
- 
- if ('(#boxzero)'.cellClick==='X' && '(#boxone)'.cellClick==='X' && '(#boxtwo)'.cellClick==='X'){
- 	console.log("we have a winner");
- return playerOne
-}
+// we will need to find winner meaning that there will need to be a row somewhere on the grid filled with either x or o, if there are not 1 row of x and 1 row of o somewhere on the board we will need to alert that it is a cats game
 
 
 
 
-}
-});//closing document ready
+
 
 //we will need to add a point to either player one or player 2 in the score box everytime a winner is declared.
 
