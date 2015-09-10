ac28bf5e72ec59548393786309e985f07e6cfcdc
Natalie Martell <nrmartell@gmail.com>
2015-09-10 09:45:23 -0500

    switches between x and o

--- tictactoe.js
+++ tictactoe.js
@@ -19,29 +19,51 @@ $(document).ready(function(){
  
 
 });/*closing document ready*/
-var cell;
-var nextTurn = 'X';
 
+var cell;
+var whosNext= 'X';
+//if player tries to select a box with something in it they will get an error.
 function cellClick(cell){
-if (cell.innerHTML === 'X' || cell.innerHTML === 'O')
-{ alert ('Square has already been choosen, please select another square');
+if (cell.textContent === 'X' || cell.textContent === 'O')
+{ alert ('This one already has something in it! Pick another one.');
 return
 }
-cell.innerHTML = nextTurn;
-playersTurn();
+cell.textContent = whosNext;
+nextTurn();
 
 }
-function playersTurn(){
-if(nextTurn == 'X'){
-    nextTurn = 'O';
+function nextTurn(){
+if(whosNext == 'X'){
+    whosNext = 'O';
     }
 else {
-    nextTurn = 'X';
+    whosNext = 'X';
     }
-
 }
 
+$(document).ready(function(){
+// we will need to find winner, if there are not 1 row of x and 1 row of o somewhere on the board we will need to alert that it is a cats game
+var playerOne= 'X';
+var playerTwo= 'O';
+
+
+function getWinner(playerOne,playerTwo){
  
+ if ('(#boxzero)'.cellClick==='X' && '(#boxone)'.cellClick==='X' && '(#boxtwo)'.cellClick==='X'){
+ 	console.log("we have a winner");
+ return playerOne
+}
+
+
+
+
+}
+});//closing document ready
+
+//we will need to add a point to either player one or player 2 in the score box everytime a winner is declared.
+
+
+//after game is either won or there is draw player will need to be able to click new game button and reset the board like new
 
 
 
